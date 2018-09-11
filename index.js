061b7237131ac19e34cd49442eae56921bc15029
DamBai212 <damanybailey@icloud.com>
2018-09-11 21:36:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,10 +20,10 @@ function switchOnCharmFromTip(tip){
  switch(tip){
    case 'generous':
     return 'Thank you so much.'
-    break;
-  case 'not as generous':
-  return 'Thank you.';
-  break;
-  default:
-return 'Bye.'
+     break;
+   case 'not as generous':
+    return 'Thank you.';
+     break;
+   default:
+    return 'Bye.'
  }}
